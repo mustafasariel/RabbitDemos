73c8d3e64211bc33404150d24780cdd3d7c58785
mustafa sarıel <mustafasariel@hotmail.com>
2020-01-13 00:52:58 +0300

    Add project files.

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread.Sleep(2000);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://yelfgfvk:YeJbM5bbqm-4T0Q8GCLgv06TspxeHymi@jaguar.rmq.cloudamqp.com/yelfgfvk");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume("hello", true, consumer);// silinmemesi için false yapmak gerekir.
+
+                    consumer.Received += Consumer_Received;
+
+                    
+                }
+            }
+            Console.WriteLine("Çıkmak için tıklayınız");
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine(message) ;
+        }
+    }
+}
---
+++ RabbitDemoPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitDemoPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // yelfgfvk
+            //YeJbM5bbqm-4T0Q8GCLgv06TspxeHymi
+            //amqp://   	amqp://yelfgfvk:YeJbM5bbqm-4T0Q8GCLgv06TspxeHymi@jaguar.rmq.cloudamqp.com/yelfgfvk
+
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://yelfgfvk:YeJbM5bbqm-4T0Q8GCLgv06TspxeHymi@jaguar.rmq.cloudamqp.com/yelfgfvk");
+
+            using (var connection=factory.CreateConnection())
+            {
+                using (var channel=connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string messages = "2. mesaj";
+                    var bodyByte = Encoding.UTF8.GetBytes(messages);
+
+                    channel.BasicPublish("", routingKey: "hello", null, bodyByte);
+
+                    Console.WriteLine("Mesaj gönderildi");
+                }
+            }
+            Console.WriteLine("Çıkmak için tıklayınız");
+            Console.ReadLine();
+        }
+    }
+}
